ddf93bc58c6339af291d28288f2719146d9a5cd8
lea <lea.bugaut@edu.devinci.fr>
2023-03-26 08:40:20 +0000

    td5

--- python_script.py
+++ python_script.py
@@ -4,6 +4,6 @@ url = 'https://api.zoopla.co.uk/api/v1/property_listings.json?county=Derbyshire'
 
 response = requests.get(url)
 
-place_ids = [place['place_id'] for place in response.jason()['listing']]
+place_ids = [place['place_id'] for place in response.json()['listing']]
 
 print(place_ids)
